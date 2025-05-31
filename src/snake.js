3ef14429e23067049918a936fb264f999a56d7dd
jjsnot <pve0069@gmail.com>
2025-05-31 13:16:18 +0200

    add Game Over conditions

--- src/snake.js
+++ src/snake.js
@@ -5,16 +5,22 @@ var cols = 20;
 var board;
 var context;
 
-//snake
+//snake head
 var snakeX = blockSize * 5
 var snakeY = blockSize * 5
 
 var velocityX = 0
 var velocityY = 0
 
+//snake body
+var snakeBody = [];
+
 //food
 var foodX = blockSize * 10
 var foodY = blockSize * 10
+
+//gameOver
+var gameOver = false
 window.onload = function () {
     board = document.getElementById("board")
     board.height = rows * blockSize
@@ -27,6 +33,9 @@ window.onload = function () {
 }
 
 function update(){
+    if(gameOver){
+        return;
+    }
     context.fillStyle = "black";
     context.fillRect(0 , 0 , board.width  , board.height)
 
@@ -34,13 +43,35 @@ function update(){
     context.fillRect(foodX ,foodY , blockSize , blockSize)
 
     if(snakeX == foodX && snakeY == foodY){
+        snakeBody.push([foodX , foodY])
         placeFood();
+    }
+    for(let i = snakeBody.length - 1; i > 0 ;i--){
+        snakeBody[i] = snakeBody[i -1]
+    }
+    if(snakeBody.length){
+        snakeBody[0] = [snakeX , snakeY]
     }
 
     context.fillStyle = "lime"
     snakeX += velocityX * blockSize;
     snakeY += velocityY * blockSize;
     context.fillRect(snakeX , snakeY ,blockSize , blockSize )
+    for(let i = 0 ; i < snakeBody.length ; i++){
+        context.fillRect(snakeBody[i][0] , snakeBody[i][1] ,blockSize , blockSize )
+
+        //game over conditions
+        if(snakeX < 0 || snakeX > cols * blockSize || snakeY < 0 || snakeY > rows * blockSize){
+            gameOver = true;
+            alert("GAME OVER!")
+        }
+        for(let i = 0; i < snakeBody.length; i++){
+            if(snakeX == snakeBody[i][0] && snakeY == snakeBody[i][1]){
+                gameOver = true
+                alert("GAME OVER!")
+            }
+        }
+    }
 
 
 
@@ -64,7 +95,7 @@ function ChangeDirection(e){
         velocityY = 0;
 
     }
-    else if(e.code == "ArrowRight" && velocityX != -1){
+    else if(e.code == "ArrowRight" && velocityX != -1 ){
         velocityX = 1;
         velocityY = 0;
 
