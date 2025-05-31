f0f568fba39dc7830cc4e29359b00b427a06e9f2
jjsnot <pve0069@gmail.com>
2025-05-31 12:23:33 +0200

    upgrade ChangeDirection and update

--- src/snake.js
+++ src/snake.js
@@ -9,6 +9,9 @@ var context;
 var snakeX = blockSize * 5
 var snakeY = blockSize * 5
 
+var velocityX = 0
+var velocityY = 0
+
 //food
 var foodX = blockSize * 10
 var foodY = blockSize * 10
@@ -18,19 +21,54 @@ window.onload = function () {
     board.width = cols * blockSize
     context = board.getContext("2d")
     placeFood()
-    update();
+    document.addEventListener("keyup" , ChangeDirection);
+    //update();
+    setInterval(update , 1000/10)
 }
 
 function update(){
     context.fillStyle = "black";
     context.fillRect(0 , 0 , board.width  , board.height)
 
-    context.fillStyle = "lime"
-    context.fillRect(snakeX , snakeY ,blockSize , blockSize )
-
     context.fillStyle = "red"
     context.fillRect(foodX ,foodY , blockSize , blockSize)
 
+    if(snakeX == foodX && snakeY == foodY){
+        placeFood();
+    }
+
+    context.fillStyle = "lime"
+    snakeX += velocityX * blockSize;
+    snakeY += velocityY * blockSize;
+    context.fillRect(snakeX , snakeY ,blockSize , blockSize )
+
+
+
+
+
+}
+
+function ChangeDirection(e){
+    if(e.code == "ArrowUp" && velocityY != 1){
+        velocityX = 0;
+        velocityY = -1;
+
+    }
+    else if(e.code == "ArrowDown" && velocityY != -1){
+        velocityX = 0;
+        velocityY = 1;
+
+    }
+    else if(e.code == "ArrowLeft" && velocityX != 1){
+        velocityX = -1;
+        velocityY = 0;
+
+    }
+    else if(e.code == "ArrowRight" && velocityX != -1){
+        velocityX = 1;
+        velocityY = 0;
+
+    }
 
 }
 
